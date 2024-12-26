88b13c58f0ebf4c8b16396504a6295a9fea47af0
Rui Li <71556158+NickyS1mpe@users.noreply.github.com>
2024-12-26 00:37:25 -0800

    first commit

---
+++ let/lib/interp.ml
@@ -0,0 +1,51 @@
+open Ast
+open Parser
+open Ds
+
+let rec eval_expr : expr -> exp_val ea_result =
+  fun e ->
+  match e with
+  | Int(n) ->
+    return (NumVal n)
+  | Var(id) ->
+    apply_env id
+  | Add(e1,e2) ->
+    eval_expr e1 >>=
+    int_of_numVal >>= fun n1 ->
+    eval_expr e2 >>=
+    int_of_numVal >>= fun n2 ->
+    return (NumVal (n1+n2))
+  | Sub(e1,e2) ->
+    eval_expr e1 >>=
+    int_of_numVal >>= fun n1 ->
+    eval_expr e2 >>=
+    int_of_numVal >>= fun n2 ->
+    return (NumVal (n1-n2))
+  | Mul(e1,e2) ->
+    eval_expr e1 >>=
+    int_of_numVal >>= fun n1 ->
+    eval_expr e2 >>=
+    int_of_numVal >>= fun n2 ->
+    return (NumVal (n1*n2))
+  | Div(e1,e2) ->
+    eval_expr e1 >>=
+    int_of_numVal >>= fun n1 ->
+    eval_expr e2 >>=
+    int_of_numVal >>= fun n2 ->
+    if n2==0
+    then error "Division by zero"
+    else return (NumVal (n1/n2))
+  | Let(id,def,body) ->
+    eval_expr def >>= 
+    extend_env id >>+
+    eval_expr body 
+
+let eval_prog (AProg(_,e)) =
+  eval_expr e
+
+let interp (e:string) : exp_val result =
+  let c = e |> parse |> eval_prog
+  in run c
+
+
+
